d79aa2c79c00e9f7489015b5b9d08617981289d6
Karanjot Singh <karanjot5665@gmail.com>
2021-04-10 01:33:06 +0530

    Add files via upload

---
+++ sortsearch/HEAPSORT.C
@@ -0,0 +1,80 @@
+
+
+#include <stdio.h>
+#include <conio.h>
+
+void makeheap ( int [ ], int ) ;
+void heapsort ( int [ ], int ) ;
+
+void main( )
+{
+	int arr[7] = { 11, 2, 9, 13, 57, 25, 17 } ;
+	int i ;
+
+	clrscr( ) ;
+	printf ( "Heap Sort.\n" ) ;
+
+	makeheap ( arr, 7 ) ;
+
+	printf ( "\nBefore Sorting:\n" ) ;
+	for ( i = 0 ; i <= 6 ; i++ )
+		printf ( "%d\t", arr[i] ) ;
+
+	heapsort ( arr, 7 ) ;
+
+	printf ( "\nAfter Sorting:\n" ) ;
+	for ( i = 0 ; i <= 6 ; i++ )
+		printf ( "%d\t", arr[i] ) ;
+
+	getch( );
+}
+
+void makeheap ( int x[ ], int n )
+{
+	int i, val, s, f ;
+	for ( i = 1 ; i < n ; i++ )
+	{
+		val = x[i] ;
+		s = i ;
+		f = ( s - 1 ) / 2 ;
+		while ( s > 0 && x[f] < val )
+		{
+			x[s] = x[f] ;
+			s = f ;
+			f = ( s - 1 ) / 2 ;
+		}
+		x[s] = val ;
+	}
+}
+
+void heapsort ( int x[ ], int n )
+{
+	int i, s, f, ivalue ;
+	for ( i = n - 1 ; i > 0 ; i-- )
+	{
+		ivalue = x[i] ;
+		x[i] = x[0] ;
+		f = 0 ;
+
+		if ( i == 1 )
+			s = -1 ;
+		else
+			s = 1 ;
+
+		if ( i > 2 && x[2] > x[1] )
+			s = 2 ;
+
+		while ( s >= 0 && ivalue < x[s] )
+		{
+			x[f] = x[s] ;
+			f = s ;
+			s = 2 * f + 1 ;
+
+			if ( s + 1 <= i - 1 && x[s] < x[s + 1] )
+				s++ ;
+			if ( s > i - 1 )
+				s = -1 ;
+		}
+		x[f] = ivalue ;
+	}
+}
---
+++ sortsearch/quicksort.c
@@ -0,0 +1,66 @@
+
+#include <stdio.h>
+#include <conio.h>
+
+int split ( int*, int, int ) ;
+void quicksort ( int *, int, int ) ;
+
+void main( )
+{
+	int arr[6] = { 11, 2, 9, 13, 57, 3 } ;
+	int i ;
+
+	clrscr( ) ;
+	printf ( "Quick sort.\n" ) ;
+	printf ( "\nArray before sorting:\n") ;
+	for ( i = 0 ; i <= 5 ; i++ )
+		printf ( "%d\t", arr[i] ) ;
+	quicksort ( arr, 0, 5 ) ;
+	printf ( "\nArray after sorting:\n") ;
+	for ( i = 0 ; i <= 5 ; i++ )
+		printf ( "%d\t", arr[i] ) ;
+	getch( ) ;
+}
+
+void quicksort ( int a[ ], int lower, int upper )
+{
+	int i ;
+	if ( upper > lower )
+	{
+		i = split ( a, lower, upper ) ;
+		quicksort ( a, lower, i - 1 ) ;
+		quicksort ( a, i + 1, upper ) ;
+	}
+}
+
+int split ( int a[ ], int lower, int upper )
+{
+	int i, p, q, t ;
+
+	p = lower + 1 ;
+	q = upper ;
+	i = a[lower] ;
+
+	while ( q >= p )
+	{
+		while ( a[p] < i )
+			p++ ;
+
+		while ( a[q] > i )
+			q-- ;
+
+		if ( q > p )
+		{
+			t = a[p] ;
+			a[p] = a[q] ;
+			a[q] = t ;
+		}
+	}
+
+	t = a[lower] ;
+	a[lower] = a[q] ;
+	a[q] = t ;
+
+	
+	return q ;
+}
